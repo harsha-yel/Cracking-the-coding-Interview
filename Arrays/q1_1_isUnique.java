9e97a52eb636d3a5fce4b97ac6d100b628008698
harsha271094 <harsha271094@gmail.com>
2017-07-02 15:36:03 -0700

    fixed indentations

--- Arrays/q1_1_isUnique.java
+++ Arrays/q1_1_isUnique.java
@@ -5,19 +5,15 @@ public class q1_1_isUnique{
 		int checker=0;
 		for(int i=0;i<str.length();i++)
 		{
-			int val=str.charAt(i)-'a';
-			
+			int val=str.charAt(i)-'a';	
 			// To see the how the bit changes internally
 			//System.out.print(val+" "+ (1<<val)+" "+(checker & (1 << val)));
-			
 			if ((checker & (1 << val)) > 0) return false;
 			checker |= (1 << val);
-			
 			//System.out.println(" "+checker);
 		}
 		return true;
 	}
-
 
 	public static void main(String args[]){
 			String str[]={"haba","hars"};
