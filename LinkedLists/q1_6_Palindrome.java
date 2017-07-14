012c25090a2264421aa73571377a6fa4eedddeae
harsha271094 <harsha271094@gmail.com>
2017-07-13 23:20:09 -0700

    modified

--- LinkedLists/q1_6_Palindrome.java
+++ LinkedLists/q1_6_Palindrome.java
@@ -1,20 +1,30 @@
 import myLinkedList.*;
+import java.util.*;
 public class q1_6_Palindrome{
 
-	static boolean palindrome(LinkedList list){
-		Node head=list.head;
-		Node tail=list.tail;
-		int i=0;
-		while(i++<list.size()){
-			if(head.getData()!=tail.getData()) return false;
-			head=head.getNext();
-			tail=tail.getPrev();
+	static boolean palindrome(myLinkedList.LinkedList list){
+		myLinkedList.Node p1=list.head;
+		Stack<Integer> s=new Stack<Integer>();
+		myLinkedList.Node p2=list.head;
+		boolean flag=false;
+		while(p2!=null){
+			//In case of odd length list
+			if(p2.getNext()==null) {flag=true;break;}
+			s.push(p1.getData());
+			p1=p1.getNext();
+			p2=p2.getNext().getNext();
+		}
+		//For odd length list, middle element can be neglected for comaparison.
+		if(flag==true) p1=p1.getNext();
+		while(p1!=null){
+			if(s.pop()!=p1.getData()) return false;
+			p1=p1.getNext();
 		}
 	return true;
 	}
 	public static void main(String[] args) {
-		LinkedList list= new LinkedList();
-		int[] ar={1,2,1,2,1};
+		myLinkedList.LinkedList list= new myLinkedList.LinkedList();
+		int[] ar={1,2,3,2,1};
 		list.generateFromArray(ar);
 		System.out.println(list.getList());
 		System.out.println(palindrome(list));
